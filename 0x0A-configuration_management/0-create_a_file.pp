9aad9916100a05ba4e785427f9114e3cc33bc49a
vagava <vagava.123@gmail.com>
2021-08-13 19:48:23 -0500

    fix style

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # create a file in /tmp:
 file { '/tmp/holberton':
-  path       => '/tmp/holberton',
-  mode       => '0744',
-  owner      => 'www-data',
-  group      => 'www-data',
-  content    => 'I love Puppet'
+  ensure  => file,
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
